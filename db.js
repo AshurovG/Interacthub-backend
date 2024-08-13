cd5eaf5502244ac21899a497e1d1ad15d42e1afd
Ashurov Georgy <ashurov1308@mail.ru>
2024-08-13 17:03:22 +0300

    add redis configurations

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require("sequelize");
+const redis = require("redis");
 
 const config = require("./config/config.json")[
   process.env.NODE_ENV || "development"
@@ -18,6 +19,36 @@ const dbConf = new Sequelize(
   }
 );
 
+const redisConf = redis.createClient({
+  host: "localhost",
+  port: 6379,
+});
+
+async function handleRedisOperation(operation) {
+  await redisConf.connect();
+  try {
+    await operation();
+  } finally {
+    await redisConf.disconnect();
+  }
+}
+
+// Пример использования
+// await handleRedisOperation(async () => {
+//   const fields = ["sessionID", "lastCode"];
+//   const values = ["sessionID", "lastCode"];
+
+//   // Используем цикл для добавления каждого поля и значения
+//   for (let i = 0; i < fields.length; i++) {
+//     await redisConf.set(`${telegram}:${fields[i]}`, values[i]);
+//   }
+
+//   const result = await redisConf.get(`${telegram}:lastCode`);
+//   console.log(result);
+// });
+
 module.exports = {
   dbConf,
+  redisConf,
+  handleRedisOperation,
 };
