e8ee4753f94cc74ff321e079e86dd5726f3c5040
Ashurov Georgy <ashurov1308@mail.ru>
2024-08-18 14:04:19 +0300

    update index file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const usersRouter = require("./users/users.routes");
 const postsRouter = require("./posts/posts.routes");
+const authRouter = require("./auth/auth.routes");
 
 const app = express();
 const PORT = 8000;
@@ -11,9 +13,11 @@ const PORT = 8000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api", usersRouter);
 app.use("/api", postsRouter);
+app.use("/api", authRouter);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
